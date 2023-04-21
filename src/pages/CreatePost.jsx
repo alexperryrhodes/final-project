abf9ff05c590d53ed21edece30ef8ecad9484973
Alexandra Rhodes <atperry30@gmail.com>
2023-04-21 13:53:53 -0700

    Added state, form and connection to supabase to create post page

--- src/pages/CreatePost.jsx
+++ src/pages/CreatePost.jsx
@@ -1,12 +1,88 @@
+import React from "react";
+import { useState } from "react";
+import { supabase } from "../src/client";
 import NavBar from "../components/NavBar";
-import PostForm from "../components/PostForm";
 
 export default function CreatePost() {
+  const [post, setPost] = useState({
+    title: "",
+    content: "",
+    image_url: "",
+    upvote_count: 0,
+  });
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setPost((prev) => {
+      return {
+        ...prev,
+        [name]: value,
+      };
+    });
+  };
+
+  const createPost = async (event) => {
+    event.preventDefault();
+
+    const { error } = await supabase
+      .from("posts")
+      .insert({
+        title: post.title,
+        content: post.content,
+        image_url: post.image_url,
+        upvote_count: post.upvote_count,
+      })
+      .select();
+
+    if (error) {
+      console.log(error);
+    }
+
+    window.location = "/";
+  };
+
   return (
     <div>
       <p>CreatePost</p>
       <NavBar />
-      <PostForm />
+
+      <div>
+        <form>
+          <label>Title</label> <br />
+          <input
+            type="text"
+            id="title"
+            name="title"
+            value={crew.title}
+            onChange={handleChange}
+          />
+          <br />
+          <br />
+          <label>Content</label>
+          <br />
+          <input
+            type="test"
+            id="content"
+            name="content"
+            value={crew.content}
+            onChange={handleChange}
+          />
+          <br />
+          <br />
+          <label>Image URL</label>
+          <br />
+          <input
+            type="text"
+            id="image_url"
+            name="image_url"
+            value={crew.image_url}
+            onChange={handleChange}
+          />
+          <br />
+          <br />
+          <input type="submit" value="Submit" onClick={createCrew} />
+        </form>
+      </div>
     </div>
   );
 }
