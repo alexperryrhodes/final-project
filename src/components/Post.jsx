0040a740eb02093fc97fddce275401f706ff0e98
Alexandra Rhodes <atperry30@gmail.com>
2023-04-21 15:28:44 -0700

    Added upvote button and removed image

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,25 +1,22 @@
 import React from "react";
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
 
 export default function Post(props) {
   return (
     <div className="Post">
       <h2>{props.title}</h2>
-          <p>{props.content}</p>
-          <img src={props.image_url} width="100" height="100"/>
-          <p>{props.upvote_count}</p>
-
+      <p>{props.content}</p>
+      <p>{props.upvote_count} upvotes</p>
 
       <Link to={"edit/" + props.id}>
         <button className="postButton">Edit</button>
-          </Link>
-          
+      </Link>
 
       <Link to={"/" + props.id}>
         <button className="postButton">More Info</button>
-          </Link>
-          
+      </Link>
 
+      <button className="postButton">Upvote</button>
     </div>
   );
 }
