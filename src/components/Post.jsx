39f63b29f93c6c1b6f7b5435b4ab46ec9fb8b8fb
Alexandra Rhodes <atperry30@gmail.com>
2023-04-21 14:48:14 -0700

    Added HTML content to Posts

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,5 +1,25 @@
-export default function Post() { 
-    return (
-        <p>Post</p>
-    );
-};
+import React from "react";
+import { Link } from 'react-router-dom'
+
+export default function Post(props) {
+  return (
+    <div className="Post">
+      <h2>{props.title}</h2>
+          <p>{props.content}</p>
+          <img src={props.image_url} width="100" height="100"/>
+          <p>{props.upvote_count}</p>
+
+
+      <Link to={"edit/" + props.id}>
+        <button className="postButton">Edit</button>
+          </Link>
+          
+
+      <Link to={"/" + props.id}>
+        <button className="postButton">More Info</button>
+          </Link>
+          
+
+    </div>
+  );
+}
