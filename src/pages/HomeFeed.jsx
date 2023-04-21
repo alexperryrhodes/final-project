25a448b5ca6e322b4f54ee6f9029d31d83653d96
Alexandra Rhodes <atperry30@gmail.com>
2023-04-21 14:46:47 -0700

    Added logic to home feed to create posts and pass data

--- src/pages/HomeFeed.jsx
+++ src/pages/HomeFeed.jsx
@@ -1,14 +1,40 @@
+import React, { useState, useEffect } from 'react';
 import NavBar from "../components/NavBar";
 import Post from "../components/Post";
 import SortButton from "../components/SortButton";
 
-export default function HomeFeed() {
+export default function HomeFeed({ data }) {
+
+  console.log("d", data)
+
+  const [post, setPost] = useState([]);
+
+  useEffect(() => {
+    setPost(data);
+  }, [data]);
+
   return (
     <div>
       <p>HomeFeed</p>
       <NavBar />
       <SortButton />
-      <Post />
+
+      <div className="HomeFeed">
+        {post && post.length > 0 ? (
+          post.map((post, index) => (
+            <Post
+              key={post.id}
+              id={post.id}
+              title={post.title}
+              content={post.content}
+              image_url={post.image_url}
+              upvote_count={post.upvote_count}
+            />
+          ))
+        ) : (
+          <h3>No Posts Yet</h3>
+        )}
+      </div>
     </div>
   );
 }
