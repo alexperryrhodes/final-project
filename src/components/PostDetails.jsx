9766e87f7ff2dc260696f7b95c8b357e0e2ba422
Alexandra Rhodes <atperry30@gmail.com>
2023-04-21 17:07:40 -0700

    Added state and Supabase connection to PostDetails to allow for upvotes

--- src/components/PostDetails.jsx
+++ src/components/PostDetails.jsx
@@ -1,19 +1,42 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import { Link } from "react-router-dom";
+import { supabase } from "../client";
 
 export default function PostDetails(props) {
+
+  const [upvotes, setUpvotes] = useState(props.details.upvote_count);
+
+  const updateUpvotes = async (event) => {
+    event.preventDefault();
+    var count = upvotes + 1;
+    const { error } = await supabase
+      .from("posts")
+      .update({
+        upvote_count: count,
+      })
+      .eq("id", props.details.id);
+
+    if (error) {
+      console.log(error);
+    } else {
+      setUpvotes(count);
+      props.details.upvote_count = count
+    }
+
+    // window.location = "/";
+  };
 
   return (
     <div className="PostDetails">
       <h2>{props.details.title}</h2>
       <img src={props.details.image_url} width="100" height="100" />
       <p>{props.details.content}</p>
-      <p>{props.details.upvote_count} upvotes</p>
+      <p>{upvotes} upvotes</p>
 
       <Link to={"edit/" + props.details.id}>
         <button className="postButton">Edit</button>
       </Link>
-      <button className="postButton">Upvote</button>
+      <button className="postButton" onClick={updateUpvotes}>Upvote</button>
     </div>
   );
 }
