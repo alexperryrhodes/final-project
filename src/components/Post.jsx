ae7db28e703a1902a85d04e1716b22cc04ff2522
Alexandra Rhodes <atperry30@gmail.com>
2023-04-21 17:07:13 -0700

    Changed what information is displayed for Post

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -5,18 +5,12 @@ export default function Post(props) {
   return (
     <div className="Post">
       <h2>{props.title}</h2>
-      <p>{props.content}</p>
+      <p>{new Date(props.created_at).toDateString()}</p>
       <p>{props.upvote_count} upvotes</p>
-
-      <Link to={"edit/" + props.id}>
-        <button className="postButton">Edit</button>
-      </Link>
 
       <Link to={"/" + props.id}>
         <button className="postButton">More Info</button>
       </Link>
-
-      <button className="postButton">Upvote</button>
     </div>
   );
 }
